60f6d59c4d8d7f6a36e434a32980e7589af41b45
yohelamat <yohel.amat@uip.edu.pa>
2025-10-25 18:34:10 +0000

    Agrego el codigo principal al main con el nombre, edad y cuidad

---
+++ main.py
@@ -0,0 +1,15 @@
+
+print("Bienvenido al Acto 1: El Comienzo de la Aventura")
+print("Antes de iniciar tu misión, necesitamos unos datos.\n")
+
+nombre = input("¿Cuál es tu nombre?: ")
+edad = input("¿Cuántos años tienes?: ")
+ciudad = input("¿En qué ciudad vives actualmente?: ")
+
+
+print( f"\nHola {nombre}, tienes {edad} años y vives en {ciudad}.")
+
+print("\nTu primera misión:")
+print("Un misterioso mensaje ha llegado desde el antiguo reino de Pythonia.")
+print("Debes dirigirte al Bosque de los Datos Perdidos para recuperar el código sagrado.")
+print("Prepárate, tu aventura apenas comienza...")
